eb0f59799d8a75c0ab8e7f88b5574b619ebd7f4a
lataviahenry77 <HenryLatavia67@gmail.com>
2026-09-17 22:45:51 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+readings = [
+    {"name": "front-door", "room": "hall",    "temp": 27.4, "online": True},
+    {"name": "hall-lamp",  "room": "hall",    "temp": 26.1, "online": True},
+    {"name": "attic",      "room": "attic",   "temp": 31.9, "online": True},
+    {"name": "fridge",     "room": "kitchen", "temp": 4.2,  "online": False},
+    {"name": "patio",      "room": "outside", "temp": 29.8, "online": True},
+]
+
+
+def hottest(devices):
+    hot = devices[0]
+    for d in devices:
+        if d["temp"] > hot["temp"]:
+            hot = d
+    return hot
+
+
+def average_temp(devices):
+    total = 0
+    for d in devices:
+        total += d["temp"]
+    return total / len(devices)
